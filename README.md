2384bcd80db8a6114dc8f0e288648ec414a25ed7
Michel Pereira <michel.augusto@pg.inatel.br>
2021-03-12 08:24:18 -0300

    setup based on the Kent C. Dodds workshop

---
+++ README.md
@@ -0,0 +1,3 @@
+# Testing Node.js Apps
+
+This is a workshop repo from [Kent C. Dodds](https://kentcdodds.com)!
---
+++ test/jest.config.projects.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  roots: [path.join(__dirname, '../src')],
+  rootDir: path.join(__dirname, '..'),
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**'],
+  moduleDirectories: [
+    'node_modules',
+    __dirname,
+    path.join(__dirname, '../src'),
+  ],
+  coverageDirectory: path.join(__dirname, '../coverage/collective'),
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coveragePathIgnorePatterns: ['.*/__tests__/.*'],
+  projects: [
+    require.resolve('./jest.config.exercises'),
+  ],
+  watchPlugins: [
+    require.resolve('jest-watch-select-projects'),
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
+}
