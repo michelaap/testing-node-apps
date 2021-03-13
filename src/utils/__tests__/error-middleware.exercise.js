3edbb26cb3185a623152947c06f56fc0af58b926
Michel Pereira <michel.augusto@pg.inatel.br>
2021-03-13 09:32:44 -0300

    write a unit test for handling headersSent in an Error Middleware

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -3,7 +3,6 @@
 import {UnauthorizedError} from 'express-jwt'
 import errorMiddleware from '../error-middleware'
 
-// 🐨 Write a test for the UnauthorizedError case
 test('responds with 401 for express-jwt UnauthorizedError', () => {
   const code = 'some_error_code'
   const message = 'Some message'
@@ -23,6 +22,21 @@ test('responds with 401 for express-jwt UnauthorizedError', () => {
   expect(res.json).toHaveBeenCalledTimes(1)
 })
 
-// 🐨 Write a test for the headersSent case
+test('calls next if headersSent is true', () => {
+  const error = new Error('blah')
+  const req = {}
+  const res = {
+    json: jest.fn(() => res),
+    status: jest.fn(() => res),
+    headersSent: true,
+  }
+  const next = jest.fn()
+
+  errorMiddleware(error, req, res, next)
+  expect(next).toHaveBeenCalledWith(error)
+  expect(next).toHaveBeenCalledTimes(1)
+  expect(res.status).not.toHaveBeenCalled()
+  expect(res.json).not.toHaveBeenCalled()
+})
 
 // 🐨 Write a test for the else case (responds with a 500)
